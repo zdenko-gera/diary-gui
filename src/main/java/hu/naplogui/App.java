85328fee1359437f42d53bcd6996c275a1ed17bf
Gera Zdenkó <h157872@stud.u-szeged.hu>
2024-05-20 03:28:41 +0200

    Minimális menü

--- src/main/java/hu/naplogui/App.java
+++ src/main/java/hu/naplogui/App.java
@@ -2,7 +2,8 @@ package hu.naplogui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,12 +15,29 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        VBox root = new VBox();
+        var appTitle = new Label("#happydiary");
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Menu noteMenu = new Menu("Jegyzetek");
+        //Menu userMenu = new Menu("Felhasználó");
+        MenuItem mi = new MenuItem("avasas");
+        MenuBar mainMenuBar = new MenuBar();
+        noteMenu.getItems().add(mi);
+        mainMenuBar.getMenus().addAll(noteMenu/*,userMenu*/);
+
+        mi.setOnAction(e-> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Cím");
+            alert.setHeaderText("Az ablak tartalom felső header része");
+            alert.setContentText("Részletesebb leírás a header text alatt");
+
+            alert.showAndWait();
+        });
+
+        root.getChildren().addAll(mainMenuBar, appTitle);
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
+        stage.setTitle("Happy Diary");
         stage.show();
     }
 
